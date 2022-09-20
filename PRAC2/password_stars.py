3479b9e605f80f2247a5f969e7b58ed1754a1fdc
viktor <rory.reid@my.jcu.edu.au>
2022-09-20 15:10:10 +1000

    Refactor password check program to use functions

--- PRAC2/password_stars.py
+++ PRAC2/password_stars.py
@@ -8,9 +8,25 @@ Example: if the user enters Pythonista (10 characters), the program should print
 
 MIN_LENGTH = 3
 
-password = input("Please enter a valid password (minimum 3 characters in length): ")
-while len(password) < MIN_LENGTH:
-    print("Password is not valid")
+def main():
+    """main function"""
+    password = get_password()
+    display_asterisks(password)
+
+
+def display_asterisks(password):
+    """print asterisks length of user input"""
+    for i in range(len(password)):
+        print("*", end=" ")
+
+
+def get_password():
+    """get password and error check"""
     password = input("Please enter a valid password (minimum 3 characters in length): ")
-for i in range(len(password)):
-    print("*", end=" ")
+    while len(password) < MIN_LENGTH:
+        print("Password is not valid")
+        password = input("Please enter a valid password (minimum 3 characters in length): ")
+    return password
+
+
+main()
