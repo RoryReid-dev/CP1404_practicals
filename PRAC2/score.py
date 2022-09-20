017f3acf1e9dcff95ece25e9e9c0a3bc85946319
viktor <rory.reid@my.jcu.edu.au>
2022-09-20 15:48:31 +1000

    update scores with function and random generation

--- PRAC2/score.py
+++ PRAC2/score.py
@@ -1,17 +1,33 @@
 """
-CP1404 - Practical 2.broken_score
-Broken program to determine score status
-The intention is that the score must be between 0 and 100 inclusive; 90 or more is excellent; 50 or more is a pass;
+CP1404 - Practical score.py
+updated with function
+Program to determine score status
+The intention is that the score must be between 0 and 100 inclusive;
+90 or more is excellent;
+50 or more is a pass;
 below 50 is bad.
-Be very careful of your boundary conditions... and test systematically.
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+import random
+
+
+def main():
+    """main function determine score results"""
+    score = float(input("Enter score: "))
+    print(determine_valid_score(score))
+    print(determine_valid_score(random.randint(0, 100)))
+
+
+def determine_valid_score(score):
+    """check if score is valid"""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
