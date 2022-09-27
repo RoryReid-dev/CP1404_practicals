d72bca54d5d6a641776347b793192b037fd0370d
Rory Reid <rory.reid@my.jcu.edu.au>
2022-09-27 10:33:11 +1000

    Answered questions to randoms.py

---
+++ prac_03/randoms.py
@@ -0,0 +1,26 @@
+"""
+In your console, type in the following (run each line multiple times),
+and write the answers to the questions below in comments in randoms.py.
+
+import random
+
+print(random.randint(5, 20))  # line 1
+print(random.randrange(3, 10, 2))  # line 2
+print(random.uniform(2.5, 5.5))  # line 3
+"""
+
+import random
+
+print(random.randint(5, 20))  # line 1
+print(random.randrange(3, 10, 2))  # line 2
+print(random.uniform(2.5, 5.5))  # line 3
+
+# What did you see on line 1? 14, 9, 7, 16, 17
+# What was the smallest number you could have seen, what was the largest? 5 and 20
+
+# What did you see on line 2? 9, 3, 7, 5, 3
+# What was the smallest number you could have seen, what was the largest? 3 and 9
+# Could line 2 have produced a 4? no, a step of 2 in range 3 - 10 cannot produce a 4
+
+# What did you see on line 3? 2.9452577171898806, 4.371899316974523, 5.0014883283002405, 5.109655683106007, 2.8558150155813005
+# What was the smallest number you could have seen, what was the largest? 2.50000000000 and 5.50000000000
