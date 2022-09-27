d0077f3c241e3465fd42c0b27d464a447feeb56b
Rory Reid <rory.reid@my.jcu.edu.au>
2022-09-28 09:20:39 +1000

    Add special character check in valid password function

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -46,10 +46,14 @@ def is_valid_password(password):
         if char in SPECIAL_CHARACTERS:
             count_special += 1
 
-    # TODO: if any of the 'normal' counts are zero, return False
+    if count_lower == 0 or count_upper == 0 or count_digit == 0:
+        return False
 
-    # TODO: if special characters are required, then check the count of those
+    # if special characters are required, then check the count of those
     # and return False if it's zero
+    if SPECIAL_CHARS_REQUIRED:
+        if count_special == 0:
+            return False
 
     # if we get here (without returning False), then the password must be valid
     return True
