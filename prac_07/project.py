22c1bf33f1616a9c1a8363acd5089efbb4796718
viktor <rory.reid@my.jcu.edu.au>
2022-11-05 22:19:35 +1000

    added load and save function, added __str__

--- prac_07/project.py
+++ prac_07/project.py
@@ -6,12 +6,22 @@ Estimate - 90 minutes
 
 
 class Project:
+    """Class to represent project"""
 
-    def __init__(self, name="", start_date=0, priority=0, cost=0.0, completion=0.0):
+    def __init__(self, name="", start_date="", priority=0, cost=0.0, completion=0.0):
+        """Initialise project class"""
         self.name = name
         self.start_date = start_date
         self.priority = priority
         self.cost = cost
         self.completion = completion
 
+    def __str__(self):
+        """string method for project class"""
+        return f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: ${self.cost}, " \
+               f"completion : {self.completion}%"
+
     def __repr__(self):
+        """repr method for project class"""
+        return f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: ${self.cost}, " \
+               f"completion : {self.completion}%"
--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -4,19 +4,26 @@ project management main program
 """
 
 import datetime
+from operator import attrgetter
+from project import Project
 
 MENU = "- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by date\n- (A)dd new " \
        "project\n- (U)pdate project\n- (Q)uit "
+FILENAME = "projects.txt"
 
 
 def main():
+    projects = []
+    load_file(projects, FILENAME)
     print(MENU)
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "L":
-            print("L")
+            user_load_file = input("Enter file name to load: ")
+            load_file(projects, user_load_file)
         elif choice == "S":
-            print("S")
+            user_save_file = input("Enter File name to save: ")
+            save_file(projects, user_save_file)
         elif choice == "D":
             print("D")
         elif choice == "F":
@@ -30,6 +37,26 @@ def main():
         print(MENU)
         choice = input(">>> ").upper()
     print("Thank you for using custom-built project management software.")
+
+
+def load_file(projects, file):
+    """Loads projects.txt."""
+    with open(FILENAME, "r") as in_file:
+        in_file.readline()  # Reads the header line
+        for line in in_file:
+            parts = line.strip().split("\t")
+            project = Project(parts[0], parts[1], int(parts[2]), float(parts[3]), int(parts[4]))
+            projects.append(project)
+
+
+def save_file(projects, file):
+    """Saves projects to projects.txt"""
+    with open(FILENAME, "w") as out_file:
+        # Add headers to file
+        print("Name\tStart Date\tPriority\tCost Estimate\tCompletion Percentage", file=out_file)
+        for project in projects:
+            print(project.name, project.start_date, project.priority, project.cost_estimate, project.completion,
+                  file=out_file, sep="\t")
 
 
 main()
