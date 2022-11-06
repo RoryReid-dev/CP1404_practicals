d2fae5ac0e2549ac31b16b1682ef6acdcbcdf594
viktor <rory.reid@my.jcu.edu.au>
2022-11-06 11:26:27 +1000

    added display and filter display options

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -7,12 +7,13 @@ import datetime
 from operator import attrgetter
 from project import Project
 
-MENU = "- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by date\n- (A)dd new " \
-       "project\n- (U)pdate project\n- (Q)uit "
+MENU = "- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by date\n" \
+       "- (A)dd new project\n- (U)pdate project\n- (Q)uit "
 FILENAME = "projects.txt"
 
 
 def main():
+    """main function for project management programme"""
     projects = []
     load_file(projects, FILENAME)
     print(MENU)
@@ -25,9 +26,10 @@ def main():
             user_save_file = input("Enter File name to save: ")
             save_file(projects, user_save_file)
         elif choice == "D":
-            print("D")
+            display_projects(projects)
         elif choice == "F":
-            print("F")
+            filter_date = input("Show projects that start after date (dd/mm/yy): ")
+            display_filtered_projects(projects, filter_date)
         elif choice == "A":
             print("A")
         elif choice == "U":
@@ -40,13 +42,35 @@ def main():
     print("Thank you for using custom-built project management software.")
 
 
+def display_filtered_projects(projects, filter_date):
+    """Displays projects after inputted filter date """
+    filter_date = datetime.datetime.strptime(filter_date, "%d/%m/%Y").date()
+    filtered_projects = [project for project in projects if project.start_date > filter_date]
+    filtered_projects.sort(key=attrgetter("start_date"))
+    for project in filtered_projects:
+        print(project)
+
+
 def display_projects(projects):
     """Displays numbered list of projects."""
     for i, project in enumerate(projects):
         print(i, project)
 
 
+def display_project_status(projects):
+    """Displays projects split in completed incomplete and ordered priority."""
+    print("Incomplete projects:")
+    for project in sorted(projects):
+        if not project.is_complete():
+            print(" ", project)
+    print("Completed projects:")
+    for project in sorted(projects):
+        if project.is_complete():
+            print(" ", project)
+
+
 def update_project(projects):
+    """update completion and priority for projects """
     project_index = int(input("Project choice: "))
     project = projects[project_index]
     print(project)
@@ -58,7 +82,7 @@ def update_project(projects):
 
 def load_file(projects, file):
     """Loads projects.txt."""
-    with open(FILENAME, "r") as in_file:
+    with open(file, "r") as in_file:
         in_file.readline()  # Reads the header line
         for line in in_file:
             parts = line.strip().split("\t")
@@ -68,7 +92,7 @@ def load_file(projects, file):
 
 def save_file(projects, file):
     """Saves projects to projects.txt"""
-    with open(FILENAME, "w") as out_file:
+    with open(file, "w") as out_file:
         # Add headers to file
         print("Name\tStart Date\tPriority\tCost Estimate\tCompletion Percentage", file=out_file)
         for project in projects:
